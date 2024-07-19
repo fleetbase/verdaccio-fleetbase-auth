9713446f18aa2ddf302783ce473f8aa08a0ad007
Ronald A. Richardson <me@ron.dev>
2024-07-19 09:45:14 +0800

    improved auth performance and efficiency, now sends groups to fleetbase api

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -2,4 +2,5 @@ import { Config } from '@verdaccio/legacy-types';
 export interface FleetbaseRegistryAuthConfig extends Config {
     fleetbaseHost: string;
     fleetbaseApiKey: string;
+    protectedPrefixes: string;
 }
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,7 +5,10 @@ export default class FleetbaseAuthPlugin implements IPluginAuth<Config> {
     private config;
     private fleetbaseClient;
     private logger;
+    private protectedPrefixes;
+    private defaultProtectedPrefixes;
     constructor(config: Config, options: any);
+    isNotProtectedPackage(packageName: string): boolean;
     authenticate(identity: string, password: string, callback: Callback): Promise<void>;
     adduser(identity: string, password: string, callback: Callback): Promise<void>;
     allow_access(user: RemoteUser, pkg: (Config & PackageAccess) | (AllowAccess & PackageAccess), callback: AuthAccessCallback): Promise<void>;
